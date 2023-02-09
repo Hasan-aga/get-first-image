bd0dd298d4dd6f70aaa81791cf1b7a24d5a71187
Hasan-aga <careerforma@gmail.com>
2023-02-09 19:51:28 +0300

    improve example

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { getFirstImageOfPage } from "./scrapeImage.js";
 
-const link = process.argv.slice(2)[0];
+const link = process.argv.slice(2)[0]
+  ? process.argv.slice(2)[0]
+  : "http://blog.hasan.one/programming/web/2023-01-30-Display-temporary-message-using-react-hooks.html";
 console.log("result", await getFirstImageOfPage(link)); // logs image link
